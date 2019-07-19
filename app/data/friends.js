4b1572ae43b7bc0c495fb42e9aeae7a8c5ee7d9f
bleauwonder <elyzableau.om@gmail.com>
2019-07-19 19:46:36 -0400

    worked on absolute value calculation

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,19 @@ var friends = [
     {
       name: "yoda",
       photo: "http://28mcsxkx4yl40jkr94fg5e7o-wpengine.netdna-ssl.com/wp-content/uploads/2011/09/spongebob3.jpg",
-      scores: []
+      scores: [      
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+  ]
     }
 ];
 
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,50 @@
 var friends = require("../data/friends");
 
+// routing the apiRoutes
 module.exports = function(app) {
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
 
 app.post("/api/friends", function(req, res) {
-    var surveyResults = req.body;
-    surveyResults.friendName = surveyResults.name.replace(/\s+/g, "").toLowerCase();
+    // loop through all possible options
+    var bestMatch = {
+      name: "",
+      photo: "", 
+      matchDif: 100
+    };
 
+    // Take the result of the survey from the user and parse it
+    var surveyResults = req.body;
+    var userScores = surveyResults.scores;
+
+    var userName = surveyResults.name;
+    var userPhoto = surveyResults.photo;
+
+    // variable used to calculate difference between new user and all other users scores
+    var totalDifference = 0;
+    // loop through the friends array to get each set of scores for friends
+    for (var i = 0; i < friends.length - 1; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+
+      // loop through that friend's score and the user's score to calculate the absolute difference
+      for (var j = 0; j < 10; j++) {
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+        if (totalDifference <= bestMatch.matchDif) {
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.matchDif = totalDifference;
+        }
+      }
+    }
+    
+    surveyResults.friendName = surveyResults.name.replace(/\s+/g, "").toLowerCase();
+    // surveyResults.scores = surveyResults.scores.map(function (x) {
+    //   return parseInt(x, 10);
+    // });
+    friends.push(surveyResults);
+
+    res.json(bestMatch);
 });
 };
