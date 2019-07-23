4502acd4cc86e4e794ff90b2c32c7c7f28b45226
bleauwonder <elyzableau.om@gmail.com>
2019-07-22 22:16:05 -0400

    connected style.css

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("app/public"));
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
